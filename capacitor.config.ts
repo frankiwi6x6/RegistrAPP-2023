3b6873be45c8217cd3574f30861152bbbea4319a
KernelOS <fra.reyesj@duocuc.cl>
2023-11-25 18:32:41 -0300

    Arreglado errores de capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'RegistrAPP',
-  webDir: 'www', 
+  webDir: './www', 
   plugins: {
     BarcodeScanner: {
       CAMERA_USAGE_DESCRIPTION: "Permitir acceso a la cámara para escanear códigos QR",
